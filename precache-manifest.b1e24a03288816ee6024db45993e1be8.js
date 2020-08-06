ae545fd800ca3630ab92fcf815b20cdaf026bbfe
mohamedadham <mohamedadham27@gmail.com>
2020-08-06 17:44:47 +0200

    Updates

---
+++ precache-manifest.b1e24a03288816ee6024db45993e1be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577d263b2739a57a56e9e48c7cdf31a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "675611e55b9c647a1323",
+    "url": "/portfolio/static/css/main.af63fd4f.chunk.css"
+  },
+  {
+    "revision": "aeda8d0d0bfec7c1c0d5",
+    "url": "/portfolio/static/js/2.c0d4ec99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c0d4ec99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675611e55b9c647a1323",
+    "url": "/portfolio/static/js/main.a2126f1e.chunk.js"
+  },
+  {
+    "revision": "5d96ae9e392a39d29dcf",
+    "url": "/portfolio/static/js/runtime-main.a077f93d.js"
+  }
+]);
