c6489aa71f8b1a273884154cbfad1b09f5a142f8
mohamedadham <mohamedadham27@gmail.com>
2020-08-06 18:07:30 +0200

    fixing images

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ const Header=(props)=>{
         </button>
         <nav className="nav">
             <ul className="nav__list">
-                <li className="nav__item"><Link to="/" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')} >Home</Link></li>
-                <li className="nav__item"><a href="/#services" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>My Services</a></li>
-                <li className="nav__item"><a href="/#about" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>About me</a></li>
-                <li className="nav__item"><a href="/#work" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>My Work</a></li>
+                <li className="nav__item"><Link to="/portfolio" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')} >Home</Link></li>
+                <li className="nav__item"><a href="/portfolio#services" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>My Services</a></li>
+                <li className="nav__item"><a href="/portfolio#about" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>About me</a></li>
+                <li className="nav__item"><a href="/portfolio#work" className="nav__link" onClick={()=>  document.body.classList.remove('nav-open')}>My Work</a></li>
             </ul>
         </nav>
     </header>
